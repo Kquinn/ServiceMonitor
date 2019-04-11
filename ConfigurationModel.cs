3a33a0bea9887b1699f5705a1e273930bacb4b0c
Kevin Quinn <kquinn@inomod.com>
2019-04-11 13:35:30 -0700

    Combine Service and SMTP configuration into one model

---
+++ ConfigurationModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceMonitor
+{
+    public class ConfigurationModel
+    {
+        public List<Service> ServiceList { get; set; }
+        public SMTPConfig SMTPConfig { get; set; }
+    }
+}
