b8b15f9ebed96b43231937aeb2c226e69d1b318c
Kevin Quinn <kquinn@inomod.com>
2019-04-11 13:34:49 -0700

    Create SMTP Configuration Model

---
+++ SMTPConfig.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceMonitor
+{
+    public class SMTPConfig
+    {
+        public string ToEmail { get; set; }
+        public string FromEmail { get; set; }
+        public string Server { get; set; }
+    }
+}
